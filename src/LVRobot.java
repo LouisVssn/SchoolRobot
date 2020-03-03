307093b0986ed4d32c70d07329004be2d0daf6c7
Louis Voßen <lvossen@ukaachen.de>
2020-03-03 16:05:27 +0100

    move method

--- src/LVRobot.java
+++ src/LVRobot.java
@@ -1,7 +1,5 @@
-import java.util.Random;
 
 public class LVRobot extends Creature {
-    static private Random rnd = new Random();
 
     @Override
     public void run() {
@@ -12,40 +10,18 @@ public class LVRobot extends Creature {
 
         while (true) {
             Observation[] obs = observe();
-            //Type a = obs[1].type;
             for (int i = 0; i < obs.length; i++) {
                 map[obs[i].position.x][obs[i].position.y] = obs[i];
             }
-
-
-            for (int u = 0; u < 100000; u++) {
-                moveForward();
-                if (!moveForward()) {
-                    attack();
-                    turn90Random();
-                    moveForward();
-                } else {
-                    moveForward();
-                }
+                move(obs);
                 printMap(height, width, map);
-            }
         }
+
     }
 
-    public void turn90Random() {
-        switch (rnd.nextInt(2)) {
-            case 0:
-                turnRight();
-                break;
-            case 1:
-                turnLeft();
-                break;
-        }
-    }
 
     //print Map in a StringBuilder and sets a value for a type
     public void printMap(int height, int width, Observation[][] map) {
-        String typ = ".type";
         StringBuilder textMap = new StringBuilder();
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
@@ -54,17 +30,37 @@ public class LVRobot extends Creature {
                 }
                 if (map[x][y] == null) {
                     textMap.append("#");
+                    continue;
                 }
-                //TODO: FEHLER
-                if (map[x][y].type == Type.WALL && map[x][y] != null) {
+                if (map[x][y].type.equals(Type.WALL)) {
                     textMap.append("X");
                 }
-
                 textMap.append(map[x][y]);
             }
         }
         System.out.print(textMap);
     }
+
+    public void move(Observation[]obs){
+        turnLeft();
+        int two = 2;
+        int obselenght = observe().length;
+        if (obs.length > two){
+            if (obselenght == two){
+                turnRight();
+            }
+            moveForward();
+        }else {
+            attack();
+            if (obselenght > two){
+                moveForward();
+            }else {
+                turnRight();
+                turnRight();
+            }
+        }
+    }
+
 
     @Override
     public String getAuthorName() {
